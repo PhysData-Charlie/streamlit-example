69a7257cce0a1e1e2a3ca7d3e0a3e6daad25c9c0
PhysData-Charlie <81766390+PhysData-Charlie@users.noreply.github.com>
2024-05-06 19:55:22 -0600

    Update streamlit_app.py
    update6

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,6 +42,8 @@ def graphs_page():
     st.bar_chart(bar_df, use_container_width=True)
 
 def update_data():
+  # aqui va el codigo para ejecutar el modelo
+  print('In progress')
   
 
 if __name __ == '__main__':
@@ -57,28 +59,5 @@ if __name __ == '__main__':
     elif selected_page == 'Graficos':
         stats_page()
 
-# num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-# num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-# indices = np.linspace(0, 1, num_points)
-# theta = 2 * np.pi * num_turns * indices
-# radius = indices
-
-# x = radius * np.cos(theta)
-# y = radius * np.sin(theta)
-
-# df = pd.DataFrame({
-#     "x": x,
-#     "y": y,
-#     "idx": indices,
-#     "rand": np.random.randn(num_points),
-# })
-
-# st.altair_chart(alt.Chart(df, height=700, width=700)
-#     .mark_point(filled=True)
-#     .encode(
-#         x=alt.X("x", axis=None),
-#         y=alt.Y("y", axis=None),
-#         color=alt.Color("idx", legend=None, scale=alt.Scale()),
 #         size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
 #     ))
